bd140a133357b0b23d0cdb880717c59267975561
arista <atd-help@arista.com>
2024-01-25 16:52:04 +0000

    master

---
+++ Python/array.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+band = ['John', 'Paul', 'George', 'Ringo']
+for member in band:
+print member
---
+++ Python/planet.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+list = ["Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Neptune", "Uranus"]
+ask_planet = raw_input("Which planet from the Sun would you like to know about? ")
+print (list[ask_planet], "is number", ask_planet, "from the Sun.")
